6694fdc629a5ed88e3ce34f8d2a8e674011f9c2f
wneto0 <wneto@agrichem.com.au>
2021-04-21 13:25:24 +1000

    expense endpoints

---
+++ src/controllers/expenseController.js
@@ -0,0 +1,46 @@
+const Expense = require("../models/expense");
+
+module.exports = {
+  async index(req, res) {
+    const expenses = await Expense.find();
+    return res.json(expenses);
+  },
+
+  async store(req, res) {
+    var { description, value, category, date } = req.body;
+    const newExpense = await Expense.create({
+      description,
+      value,
+      category,
+      date,
+    });
+    return res.json(newExpense);
+  },
+
+  async edit(req, res) {
+    var { _id, description, value, category, date } = req.body.expense;
+    await Expense.updateOne(
+      { _id: _id },
+      {
+        $set: {
+          description: description,
+          value: value,
+          category: category,
+          date: date,
+        },
+      }
+    );
+    const newExpense = await Expense.findById(_id);
+
+    return res.status(200).json(newExpense);
+  },
+
+  async delete(req, res) {
+    const success = await Expense.findByIdAndDelete(req.body.expense._id);
+    if (success) {
+      res.status(204).end();
+    } else {
+      res.status(404).end();
+    }
+  },
+};
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,20 @@
-const express = require('express');
+const express = require("express");
 
-const CategoryController = require('./controllers/categoryController');
+const CategoryController = require("./controllers/categoryController");
+const ExpenseController = require("./controllers/expenseController");
 
 const routes = express.Router();
 
-routes.get('/category/expense', CategoryController.getExpensesCategory);
-routes.get('/category/income', CategoryController.getIncomeCategory);
-routes.get('/category', CategoryController.index);
-routes.post('/category', CategoryController.store);
-routes.delete('/category', CategoryController.delete);
-routes.put('/category', CategoryController.edit);
+routes.get("/category/expense", CategoryController.getExpensesCategory);
+routes.get("/category/income", CategoryController.getIncomeCategory);
+routes.get("/category", CategoryController.index);
+routes.post("/category", CategoryController.store);
+routes.delete("/category", CategoryController.delete);
+routes.put("/category", CategoryController.edit);
+
+routes.post("/expense", ExpenseController.store);
+routes.get("/expense", ExpenseController.index);
+routes.delete("/expense", ExpenseController.delete);
+routes.put("/expense", ExpenseController.edit);
 
 module.exports = routes;
